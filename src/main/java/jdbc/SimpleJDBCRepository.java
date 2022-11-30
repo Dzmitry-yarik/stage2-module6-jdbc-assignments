7ecd2c46a9d6c82b68fc8717af5e52f84ca7b6d0
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-11-30 04:32:46 +0400

    solved

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -30,7 +30,7 @@ public class SimpleJDBCRepository {
         }
     }
 
-    
+
 
     private static final String createUserSQL = "INSERT INTO users (firstname, lastname, age) VALUES(?, ?, ?)";
     private static final String updateUserSQL = "UPDATE users SET firstname = ?, lastname = ?, age = ? WHERE id = ?";
@@ -42,7 +42,7 @@ public class SimpleJDBCRepository {
     public Long createUser(User user) throws SQLException {
         Long id = null;
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(CREATE_USER_SQL, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement statement = conn.prepareStatement(createUserSQL, Statement.RETURN_GENERATED_KEYS)) {
             statement.setObject(1, user.getFirstName());
             statement.setObject(2, user.getLastName());
             statement.setObject(3, user.getAge());
@@ -58,7 +58,7 @@ public class SimpleJDBCRepository {
     public User findUserById(Long userId) throws SQLException {
         User user = null;
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(FIND_USER_BY_ID_SQL)) {
+             PreparedStatement statement = conn.prepareStatement(findUserByIdSQL)) {
             statement.setLong(1, userId);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
@@ -71,7 +71,7 @@ public class SimpleJDBCRepository {
     public User findUserByName(String userName) throws SQLException {
         User user = null;
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(FIND_USER_BY_NAME_SQL)) {
+             PreparedStatement statement = conn.prepareStatement(findUserByNameSQL)) {
              statement.setString(1, userName);
              ResultSet resultSet = statement.executeQuery();
              if (resultSet.next()) {
@@ -86,7 +86,7 @@ public class SimpleJDBCRepository {
         List<User> users = new ArrayList<>();
 
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(FIND_ALL_USER_SQL)) {
+             PreparedStatement statement = conn.prepareStatement(findAllUserSQL)) {
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 users.add(map(resultSet));
@@ -97,7 +97,7 @@ public class SimpleJDBCRepository {
 
     public User updateUser(User user) throws SQLException{
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(UPDATE_USER_SQL)) {
+             PreparedStatement statement = conn.prepareStatement(updateUserSQL)) {
             statement.setString(1, user.getFirstName());
             statement.setString(2, user.getLastName());
             statement.setInt(3, user.getAge());
@@ -111,7 +111,7 @@ public class SimpleJDBCRepository {
 
     public void deleteUser(Long userId) throws SQLException{
         try (var conn = dataSource.getConnection();
-             var statement = conn.prepareStatement(DELETE_USER)) {
+             var statement = conn.prepareStatement(deleteUser)) {
             statement.setLong(1, userId);
             statement.executeUpdate();
         }
