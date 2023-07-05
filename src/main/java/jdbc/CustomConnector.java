e965bfa92a9cd6c59c4a3c4bb7a0fbb498304eb3
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-05 22:27:56 +0300

    Update CustomConnector.java

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -1,11 +1,30 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class CustomConnector {
+    public static final String MYSQL_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    
     public Connection getConnection(String url) {
+        try {
+            return DriverManager.getConnection(url);
+        } catch ( SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 
     public Connection getConnection(String url, String user, String password)  {
+            Connection connection;
+        try {
+            Class.forName(MYSQL_JDBC_DRIVER);
+/           connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+        return connection;
     }
 }
